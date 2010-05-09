4b42b54d5669e15914832bf458dd37597bba419f
Joshua Lippiner <jlippiner@noctivity.com>
2010-05-09 13:00:02 -0400

    changed schedule to run at 3am due to PST servers.  changed send_daily_email.rb to instance method due to cron issues

--- lib/jobs/send_daily_emails.rb
+++ lib/jobs/send_daily_emails.rb
@@ -1,5 +1,5 @@
 class SendDailyEmails
-  def self.process
+  def process
     User.wanting_daily_emails.each do |user|
       user.update_attribute(:daily_summary_sent_at, Time.now) if Postoffice.deliver_email_todos(user)
     end
